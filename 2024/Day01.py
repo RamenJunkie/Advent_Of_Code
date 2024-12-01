a77212aed8913432df23ffc9271a88e316bd177a
RamenJunkie <josh_miller79@outlook.com>
2024-12-01 15:04:29 -0600

    Updated via Simple CLI Push

---
+++ 2024/Day01.py
@@ -0,0 +1,30 @@
+with open("Day01Input.txt") as file:
+    data = file.read()
+
+sets = [each.split("   ") for each in data.split("\n")]
+sets.pop()
+
+list1 = []
+list2 = []
+
+for each in sets:
+   list1.append(int(each[0]))
+   list2.append(int(each[1]))
+
+list1.sort()
+list2.sort()
+
+total = 0
+total2 = 0
+for i in range(len(list1)):
+    total += abs(list1[i]-list2[i])
+    total2 += list1[i]*list2.count(list1[i])
+
+print(total)
+print(total2)
+
+#totals.sort(reverse=True)
+#print(totals[0]+totals[1]+totals[2])
+# 69177
+#207456
+
